847e486844485b43f28c7bfce218f40f296707a3
richardeascanio <rascanio@ubiipagos.com>
2020-11-26 12:29:06 -0400

    setting up bar chart

--- app/src/main/java/com/richard/runningtrackerapp/di/AppModule.kt
+++ app/src/main/java/com/richard/runningtrackerapp/di/AppModule.kt
@@ -1,9 +1,15 @@
 package com.richard.runningtrackerapp.di
 
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import androidx.room.Room
 import com.richard.runningtrackerapp.db.RunningDatabase
+import com.richard.runningtrackerapp.other.Constants.KEY_FIRST_TIME_TOGGLE
+import com.richard.runningtrackerapp.other.Constants.KEY_NAME
+import com.richard.runningtrackerapp.other.Constants.KEY_WEIGHT
 import com.richard.runningtrackerapp.other.Constants.RUNNING_DATABASE_NAME
+import com.richard.runningtrackerapp.other.Constants.SHARED_PREFERENCES_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -27,4 +33,21 @@ object AppModule {
     @Singleton
     @Provides
     fun provideDao(db: RunningDatabase) = db.getRunDao()
+
+    @Singleton
+    @Provides
+    fun provideSharedPreferences(@ApplicationContext app: Context) =
+        app.getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
+
+    @Singleton
+    @Provides
+    fun provideName(sharedPref: SharedPreferences) = sharedPref.getString(KEY_NAME, "") ?: ""
+
+    @Singleton
+    @Provides
+    fun provideWeight(sharedPref: SharedPreferences) = sharedPref.getFloat(KEY_WEIGHT, 80f)
+
+    @Singleton
+    @Provides
+    fun provideFirstTimeToggle(sharedPref: SharedPreferences) = sharedPref.getBoolean(KEY_FIRST_TIME_TOGGLE, true)
 }
--- app/src/main/java/com/richard/runningtrackerapp/ui/viewmodels/StatisticsViewModel.kt
+++ app/src/main/java/com/richard/runningtrackerapp/ui/viewmodels/StatisticsViewModel.kt
@@ -7,6 +7,14 @@ import com.richard.runningtrackerapp.repositories.MainRepository
 class StatisticsViewModel
 @ViewModelInject
 constructor(
-    val mainRepository: MainRepository
+    private val mainRepository: MainRepository
 ): ViewModel() {
+
+    val totalTimeRun = mainRepository.getTotalTimeInMillis()
+    val totalDistance = mainRepository.getTotalDistance()
+    val totalCaloriesBurned = mainRepository.getTotalCaloriesBurned()
+    val totalAvgSpeed = mainRepository.getTotalAvgSpeed()
+
+    val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
+
 }
